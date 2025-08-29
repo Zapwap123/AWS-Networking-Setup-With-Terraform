73984beeff4bc3f295e1278c009e45dd25256551
Zapwap123 <sethanmawen97@gmail.com>
2025-08-29 15:02:01 +0000

    fix: correction in referencing the igw module

--- modules/route_table/main.tf
+++ modules/route_table/main.tf
@@ -4,17 +4,15 @@ resource "aws_route_table" "this" {
   tags = merge(var.tags, {
     Name = "${var.env}-public-rt"
   })
-
 }
 
 resource "aws_route" "default_route" {
   route_table_id         = aws_route_table.this.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.this.id
+  gateway_id             = var.igw_id
 }
 
 resource "aws_route_table_association" "subnet_assoc" {
   subnet_id      = var.subnet_id
   route_table_id = aws_route_table.this.id
 }
-
