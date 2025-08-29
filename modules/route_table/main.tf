c8df0b75d73dc84de885e881faba149c4648e714
Zapwap123 <sethanmawen97@gmail.com>
2025-08-29 14:48:54 +0000

    Fix: Corrected the resource used since it was pointing to the wrong resource

--- modules/route_table/main.tf
+++ modules/route_table/main.tf
@@ -1,18 +1,20 @@
 resource "aws_route_table" "this" {
   vpc_id = var.vpc_id
 
-  tags merge(var.tags, {
+  tags = merge(var.tags, {
     Name = "${var.env}-public-rt"
   })
+
 }
 
 resource "aws_route" "default_route" {
-  route_table_id = aws_route_table.this.id
+  route_table_id         = aws_route_table.this.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.this.id
+  gateway_id             = aws_internet_gateway.this.id
 }
 
-resource "aws_ram_principal_association" "subnet_assoc" {
-  subnet_id = var.subnet_id
+resource "aws_route_table_association" "subnet_assoc" {
+  subnet_id      = var.subnet_id
   route_table_id = aws_route_table.this.id
 }
+
